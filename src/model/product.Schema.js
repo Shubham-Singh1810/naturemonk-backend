9b8e6a5aec9885e0517c7e80f7c29e8a339a8af2
Shubham <hittheshubham1810@gmail.com>
2025-05-17 16:52:02 +0530

    first commit

---
+++ src/model/product.Schema.js
@@ -0,0 +1,168 @@
+const mongoose = require("mongoose");
+const timestamps = require("mongoose-timestamp");
+
+const productSchema = mongoose.Schema({
+  // step 1
+  name: {
+    type: String,
+    required: true,
+  },
+  tags: {
+    type: [String],
+  },
+  productType: {
+    type: String,
+  },
+  tax: {
+    type: String,
+  },
+  categoryId: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+    },
+  ],
+  hsnCode: {
+    type: Number,
+  },
+  GTIN: {
+    type: Number,
+  },
+
+  shortDescription: {
+    type: String,
+  },
+
+  // step 2
+  stockQuantity: {
+    type: Number,
+  },
+  brandId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Brand",
+  },
+  price: {
+    type: Number,
+  },
+  discountedPrice: {
+    type: Number,
+  },
+  offerPrice: {
+    type: Number,
+  },
+  ingredients: {
+    type: String,
+  },
+  packOf: {
+    type: Number,
+  },
+  numberOfPieces: {
+    type: Number,
+  },
+
+  currency: {
+    type: String,
+  },
+
+  saleStartDate: {
+    type: Date,
+  },
+  saleEndDate: {
+    type: Date,
+  },
+  itemWeight: {
+    type: Number,
+  },
+  packageWeight: {
+    type: Number,
+  },
+  description: {
+    type: String,
+  },
+
+  // Nutrition Information
+  packSize: {
+    type: Number,
+  },
+  servingSize: {
+    type: Number,
+  },
+  energy: {
+    type: Number,
+  },
+  protein: {
+    type: Number,
+  },
+  carbohydrate: {
+    type: Number,
+  },
+  fat: {
+    type: Number,
+  },
+  saturatedFat: {
+    type: Number,
+  },
+  transFat: {
+    type: Number,
+  },
+  totalSugar: {
+    type: Number,
+  },
+  addedSugar: {
+    type: Number,
+  },
+  dietaryFiber: {
+    type: Number,
+  },
+  calcium: {
+    type: Number,
+  },
+  iron: {
+    type: Number,
+  },
+  phosphorus: {
+    type: Number,
+  },
+  potassium: {
+    type: Number,
+  },
+  sodium: {
+    type: Number,
+  },
+
+  // step 3
+  productHeroImage: {
+    type: String,
+  },
+
+  productGallery: {
+    type: [String],
+  },
+  productVideo: {
+    type: String,
+  },
+
+  // step 4 attributes
+  productOtherDetails: [
+    {
+      key: { type: String },
+      value: [{ type: String }],
+    },
+  ],
+  productVariants: [
+    {
+      variantKey: { type: String },
+      variantValue: { type: String },
+      variantPrice: { type: Number },
+      variantDiscountedPrice: { type: Number },
+      variantImage: { type: String },
+    },
+  ],
+  status: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+productSchema.plugin(timestamps);
+module.exports = mongoose.model("Product", productSchema);
